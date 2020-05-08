588dc26ddfdabe5334c15858ecafa3c6fe1f90ab
Christopher Hertel <mail@christopher-hertel.de>
2020-05-08 23:58:17 +0200

    dynamize interface with sample data and ajax implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,14 +11,54 @@ self.MonacoEnvironment = {
     },
 };
 
-monaco.editor.create(document.getElementById('js-editor-left'), {
-    value: ['<?php', ''].join('\n'),
-    minimap: { enabled: false },
-    language: 'php',
-});
+(function (window, $, monaco) {
+    $(document).ready(function () {
+        new ComplexityDiff();
+    });
 
-monaco.editor.create(document.getElementById('js-editor-right'), {
-    value: ['<?php', ''].join('\n'),
-    minimap: { enabled: false },
-    language: 'php',
-});
+    window.ComplexityDiff = function () {
+        new Editor($('#js-editor-left'));
+        new Editor($('#js-editor-right'));
+    };
+
+    var Editor = function ($wrapper) {
+        this.$wrapper = $wrapper;
+        let $editor = $wrapper.find('.editor');
+        let code = $editor.text();
+        $editor.html('');
+        this.editor = monaco.editor.create($editor[0], {
+            value: code,
+            minimap: { enabled: false },
+            language: 'php',
+        });
+
+        $wrapper.on(
+            'click',
+            '.js-recalculate-complexities',
+            this.handleRecalculate.bind(this)
+        );
+    };
+
+    $.extend(Editor.prototype, {
+        handleRecalculate: function () {
+            var self = this;
+            $.ajax({
+                method: 'POST',
+                url: '/calculate',
+                data: this.editor.getModel().getValue(),
+            })
+                .then(function (data) {
+                    self.$wrapper
+                        .find('.js-cyclomatic-complexity')
+                        .html(data.cyclomatic_complexity);
+                    self.$wrapper
+                        .find('.js-cognitive-complexity')
+                        .html(data.cognitive_complexity);
+                })
+                .catch(function (jqXHR) {
+                    self.$wrapper.find('.js-cyclomatic-complexity').html('X');
+                    self.$wrapper.find('.js-cognitive-complexity').html('X');
+                });
+        },
+    });
+})(window, jQuery, monaco);
