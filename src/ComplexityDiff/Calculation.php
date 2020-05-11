49346bc16e4c715aebe087c8bb38802d2828ba6c
Christopher Hertel <mail@christopher-hertel.de>
2020-05-11 17:35:39 +0200

    implementing color scheme for complexity levels

--- src/ComplexityDiff/Calculation.php
+++ src/ComplexityDiff/Calculation.php
@@ -16,13 +16,39 @@ final class Calculation implements \JsonSerializable
     }
 
     /**
-     * @return array<string,int>
+     * @return array<string, array|string>
      */
     public function jsonSerialize(): array
     {
         return [
-            'cyclomatic_complexity' => $this->cyclomaticComplexity,
-            'cognitive_complexity' => $this->cognitiveComplexity,
+            'cyclomatic_complexity' => [
+                'value' => $this->cyclomaticComplexity,
+                'level' => $this->getComplexityLevel($this->cyclomaticComplexity),
+            ],
+            'cognitive_complexity' => [
+                'value' => $this->cognitiveComplexity,
+                'level' => $this->getComplexityLevel($this->cognitiveComplexity),
+            ],
+            'complexity_level' => $this->getComplexityLevel(
+                ($this->cyclomaticComplexity + $this->cognitiveComplexity) / 2
+            ),
         ];
+    }
+
+    private function getComplexityLevel(float $value): string
+    {
+        if ($value <= 4) {
+            return 'low';
+        }
+
+        if ($value <= 7) {
+            return 'moderate';
+        }
+
+        if ($value <= 10) {
+            return 'high';
+        }
+
+        return 'very-high';
     }
 }
