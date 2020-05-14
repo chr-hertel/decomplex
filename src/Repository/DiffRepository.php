2a27130d129caa6834068e8eeb9703bfaf19c13b
Christopher Hertel <mail@christopher-hertel.de>
2020-05-14 17:14:24 +0200

    adding statistics command

---
+++ src/Repository/DiffRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Diff;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DiffRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Diff::class);
+    }
+}
