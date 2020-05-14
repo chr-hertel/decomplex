a718e142303bfc65c624862917ae69dac1d2ef28
Christopher Hertel <mail@christopher-hertel.de>
2020-05-14 17:14:08 +0200

    adding schema migration

---
+++ src/Migrations/Version20200514150850.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200514150850 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE diff (id VARCHAR(6) NOT NULL, code_snippet_left TEXT NOT NULL, code_snippet_right TEXT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE diff');
+    }
+}
