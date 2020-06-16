6703f21f83c9d40fb7c52424db9d37c8d48d88be
Christopher Hertel <mail@christopher-hertel.de>
2020-06-16 18:05:52 +0200

    split javascript code into files

---
+++ assets/js/complexity-diff.js
@@ -0,0 +1,42 @@
+import { Editor } from './editor';
+
+export let ComplexityDiff = function () {
+    this.editorLeft = new Editor($('#js-editor-left'));
+    this.editorRight = new Editor($('#js-editor-right'));
+
+    $(document).on(
+        'click',
+        '.js-permalink-create',
+        this.handlePermalinkCreate.bind(this)
+    );
+    $(document).on(
+        'click',
+        '.js-permalink-copy',
+        this.handlePermalinkCopy.bind(this)
+    );
+};
+
+$.extend(ComplexityDiff.prototype, {
+    handlePermalinkCreate: function () {
+        var self = this;
+        $.ajax({
+            method: 'POST',
+            url: '/permalink',
+            data: {
+                left: self.editorLeft.getCode(),
+                right: self.editorRight.getCode(),
+            },
+        })
+            .then(function (data) {
+                $('.js-permalink-input').val(data);
+            })
+            .catch(function (jqXHR) {
+                console.error(jqXHR);
+            });
+    },
+    handlePermalinkCopy: function () {
+        let $input = $('.js-permalink-input');
+        $input.select();
+        document.execCommand('copy');
+    },
+});
