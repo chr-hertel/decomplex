63a8ed2c6ce59156fa3cf92d53c173c9c209ac1a
Christopher Hertel <mail@christopher-hertel.de>
2022-06-06 16:37:19 +0200

    remove final keyword from entities to allow cache warmup with doctrine proxies

--- src/Entity/Snippet.php
+++ src/Entity/Snippet.php
@@ -7,7 +7,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
-final class Snippet implements \JsonSerializable
+class Snippet implements \JsonSerializable
 {
     /**
      * @psalm-suppress PropertyNotSetInConstructor
