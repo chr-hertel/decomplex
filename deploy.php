d86b06ccc98a723b8a3f3d36d9b926fda130b7ec
Christopher Hertel <mail@christopher-hertel.de>
2022-07-17 17:36:42 +0200

    switch to authoritative classmap

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/symfony.php';
 
 // Config
 set('repository', 'git@github.com:chr-hertel/decomplex.git');
-set('composer_options', '--no-dev --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction --no-scripts');
+set('composer_options', '--no-dev --verbose --prefer-dist --classmap-authoritative --no-progress --no-interaction --no-scripts');
 set('console_options', '--no-interaction --env=prod');
 
 // Hosts
